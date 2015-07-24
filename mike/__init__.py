0966e9820edeb5422512248e939a20f2645a7450
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2015-07-24 13:35:05 -0500

    autopep8

--- mike/__init__.py
+++ mike/__init__.py
@@ -1,5 +1,6 @@
 import random
-things_that_mike_sucks_at = ['apples', 'bananas','at life', 'toes']
+things_that_mike_sucks_at = ['apples', 'bananas', 'at life', 'toes']
+
 
 def sux():
     return random.choice(things_that_mike_sucks_at)
